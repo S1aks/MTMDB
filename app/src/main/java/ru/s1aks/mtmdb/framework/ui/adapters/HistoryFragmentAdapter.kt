6020a5f64fc6f1463b65e8cb470275472fd29b42
Viacheslav Khrykin <vh78aks@gmail.com>
2021-07-18 19:42:53 +0300

    Homework 12

--- app/src/main/java/ru/s1aks/mtmdb/framework/ui/adapters/HistoryFragmentAdapter.kt
+++ app/src/main/java/ru/s1aks/mtmdb/framework/ui/adapters/HistoryFragmentAdapter.kt
@@ -1,14 +1,14 @@
 package ru.s1aks.mtmdb.framework.ui.adapters
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import ru.s1aks.mtmdb.databinding.FragmentHistoryRecyclerItemBinding
+import ru.s1aks.mtmdb.framework.ui.history_fragment.HistoryFragment
 import ru.s1aks.mtmdb.model.entities.History
 
-class HistoryFragmentAdapter : RecyclerView.Adapter<HistoryFragmentAdapter.HistoryViewHolder>() {
+class HistoryFragmentAdapter(private var itemClickListener: HistoryFragment.OnItemViewClickListener?) :
+    RecyclerView.Adapter<HistoryFragmentAdapter.HistoryViewHolder>() {
     private var data: List<History> = listOf()
 
     fun setData(data: List<History>) {
@@ -17,33 +17,25 @@ class HistoryFragmentAdapter : RecyclerView.Adapter<HistoryFragmentAdapter.Histo
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = HistoryViewHolder(
-            FragmentHistoryRecyclerItemBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false
-            )
+        FragmentHistoryRecyclerItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
         )
-
+    )
 
     override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
         holder.bind(data[position])
-        Log.d("VHOLDER", data[position].toString())
     }
 
     override fun getItemCount() = data.size
 
-    inner class HistoryViewHolder(private val binding: FragmentHistoryRecyclerItemBinding)
-        : RecyclerView.ViewHolder(binding.root) {
+    inner class HistoryViewHolder(private val binding: FragmentHistoryRecyclerItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(data: History) = with(binding) {
             if (layoutPosition != RecyclerView.NO_POSITION) {
                 movieTitle.text = data.movie_title
                 time.text = data.time
-                root.setOnClickListener {
-                    Toast.makeText(
-                        itemView.context,
-                        "on click: ${data.movie_title}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                root.setOnClickListener { itemClickListener?.onItemViewClick(data.movie_id) }
             }
         }
     }
